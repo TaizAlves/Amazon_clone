171b551c8087f3da421ffa16ab8ea25bae71411c
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-09-10 19:20:10 -0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyALvjJSPMz6Rcu_xeQeTYAo6314qMgdqFs",
+    authDomain: "clone-66464.firebaseapp.com",
+    databaseURL: "https://clone-66464.firebaseio.com",
+    projectId: "clone-66464",
+    storageBucket: "clone-66464.appspot.com",
+    messagingSenderId: "670333315016",
+    appId: "1:670333315016:web:06266e2c951f25c0e682fd",
+    measurementId: "G-LEMJ3HTKK0"
+  };
+  
